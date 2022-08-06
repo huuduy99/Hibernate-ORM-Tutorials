4fd31e5f86ab175b65f0557b539fb709a6be0636
Hữu Duy <huu_duy99@yahoo.com>
2022-08-07 02:42:22 +0700

    (hibernate-get-entity-example) first run!

--- hibernate-get-entity-example/src/main/java/net/javaguides/hibernate/dao/StudentDao.java
+++ hibernate-get-entity-example/src/main/java/net/javaguides/hibernate/dao/StudentDao.java
@@ -7,83 +7,104 @@ import net.javaguides.hibernate.entity.Student;
 import net.javaguides.hibernate.util.HibernateUtil;
 
 public class StudentDao {
-	public void getStudent(int id) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
+    public void getStudent(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
 
-			// get Student entity using get() method
-			Student student = session.get(Student.class, id);
-			System.out.println(student.getFirstName());
-			System.out.println(student.getEmail());
+            // get Student entity using get() method
+            Student student = session.get(Student.class, id);
+            System.out.println(student.getFirstName());
+            System.out.println(student.getEmail());
 
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
-	public void loadStudent(int id) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
+    public void loadStudent(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
 
-			// get Student entity using load() method
-			Student student = session.load(Student.class, id);
-			System.out.println(student.getFirstName());
-			System.out.println(student.getEmail());
+            // get Student entity using load() method
+            Student student = session.load(Student.class, id);
+            System.out.println(student.getFirstName());
+            System.out.println(student.getEmail());
 
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
-	public void getStudentById(int id) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
+    public void getStudentById(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
 
-			// Obtain an entity using byId() method
-			Student student = session.byId(Student.class).getReference(id);
-			System.out.println(student.getFirstName());
-			System.out.println(student.getEmail());
+            // Obtain an entity using byId() method
+            Student student = session.byId(Student.class).getReference(id);
+            System.out.println(student.getFirstName());
+            System.out.println(student.getEmail());
 
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
-	
-	public void saveStudent(Student student) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.save(student);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void getStudentWithReferencesMethod(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // Obtain an entity using byId() method
+            Student student = session.getReference(Student.class, id);
+            System.out.println(student.getFirstName());
+            System.out.println(student.getEmail());
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void saveStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
