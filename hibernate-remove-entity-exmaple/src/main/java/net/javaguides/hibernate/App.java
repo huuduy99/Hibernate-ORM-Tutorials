b4ec0133b7b9a0465da368399e8d14a8b7df0b68
Hữu Duy <huu_duy99@yahoo.com>
2022-08-08 02:54:22 +0700

    (hibernate-remove-entity-example): first run!

--- hibernate-remove-entity-exmaple/src/main/java/net/javaguides/hibernate/App.java
+++ hibernate-remove-entity-exmaple/src/main/java/net/javaguides/hibernate/App.java
@@ -6,21 +6,21 @@ import net.javaguides.hibernate.dao.SessionSaveExample;
 import net.javaguides.hibernate.entity.Student;
 
 public class App {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// first save few records to database
-		SessionSaveExample studentDao = new SessionSaveExample();
-		Student s1 = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
-		Student s2 = new Student("John", "Cena", "johncena@javaguides.com");
-		studentDao.saveStudent(s1);
-		studentDao.saveStudent(s2);
+        // first save few records to database
+        SessionSaveExample studentDao = new SessionSaveExample();
+        Student s1 = new Student("Ramesh", "Fadatare", "rameshfadatare@javaguides.com");
+        Student s2 = new Student("John", "Cena", "johncena@javaguides.com");
+        studentDao.saveStudent(s1);
+        studentDao.saveStudent(s2);
 
-		// delete student
-		SessionDeleteExample deleteExample = new SessionDeleteExample();
-		deleteExample.deleteStudent(1);
+        // delete student
+        SessionDeleteExample deleteExample = new SessionDeleteExample();
+        deleteExample.deleteStudent(1);
 
-		// delete second student
-		/*SessionRemoveExample persistExample = new SessionRemoveExample();
-		persistExample.removeStudent(2);*/
-	}
+        // delete second student
+//        SessionRemoveExample persistExample = new SessionRemoveExample();
+//        persistExample.removeStudent(1);
+    }
 }
--- hibernate-remove-entity-exmaple/src/main/java/net/javaguides/hibernate/dao/SessionSaveExample.java
+++ hibernate-remove-entity-exmaple/src/main/java/net/javaguides/hibernate/dao/SessionSaveExample.java
@@ -7,20 +7,20 @@ import net.javaguides.hibernate.entity.Student;
 import net.javaguides.hibernate.util.HibernateUtil;
 
 public class SessionSaveExample {
-	public void saveStudent(Student student) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.save(student);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
+    public void saveStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
