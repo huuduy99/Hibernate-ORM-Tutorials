4e459a49b587b4b65eebbc4c6ae77130775e494e
Hữu Duy <huu_duy99@yahoo.com>
2022-08-08 22:20:29 +0700

    (hibernate-c3p0-pooling-example): first run!

--- hibernate-c3p0-pooling-example/src/main/java/net/javaguides/hibernate/dao/StudentDao.java
+++ hibernate-c3p0-pooling-example/src/main/java/net/javaguides/hibernate/dao/StudentDao.java
@@ -9,87 +9,87 @@ import net.javaguides.hibernate.entity.Student;
 import net.javaguides.hibernate.util.HibernateUtil;
 
 public class StudentDao {
-	public void saveStudent(Student student) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.save(student);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
+    public void saveStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
-	public void updateStudent(Student student) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
-			// save the student object
-			session.update(student);
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
-	
-	public void getStudent(int id) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
+    public void updateStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.update(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
-			// get Student entity using get() method
-			Student student = session.get(Student.class, id);
-			System.out.println(student.getFirstName());
-			System.out.println(student.getEmail());
+    public void getStudent(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
 
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
-	
-	public void deleteStudent(int id) {
-		Transaction transaction = null;
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			// start a transaction
-			transaction = session.beginTransaction();
+            // get Student entity using get() method
+            Student student = session.get(Student.class, id);
+            System.out.println(student.getFirstName());
+            System.out.println(student.getEmail());
 
-			// Delete a persistent object
-			Student student = session.get(Student.class, id);
-			if (student != null) {
-				session.delete(student);
-				System.out.println("student 1 is deleted");
-			}
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 
-			// commit transaction
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			e.printStackTrace();
-		}
-	}
-	
-	public List<Student> getStudents() {
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			return session.createQuery("from Student", Student.class).list();
-		}
-	}
+    public void deleteStudent(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // Delete a persistent object
+            Student student = session.get(Student.class, id);
+            if (student != null) {
+                session.delete(student);
+                System.out.println("student 1 is deleted");
+            }
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public List<Student> getStudents() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Student", Student.class).list();
+        }
+    }
 }
--- hibernate-c3p0-pooling-example/src/main/java/net/javaguides/hibernate/util/HibernateUtil.java
+++ hibernate-c3p0-pooling-example/src/main/java/net/javaguides/hibernate/util/HibernateUtil.java
@@ -9,43 +9,44 @@ import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 import net.javaguides.hibernate.entity.Student;
+
 public class HibernateUtil {
-	private static SessionFactory sessionFactory;
-	public static SessionFactory getSessionFactory() {
-		if (sessionFactory == null) {
-			try {
-				Configuration configuration = new Configuration();
-				// Hibernate settings equivalent to hibernate.cfg.xml's properties
-				Properties settings = new Properties();
-				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false");
-				settings.put(Environment.USER, "root");
-				settings.put(Environment.PASS, "root");
-				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-				settings.put(Environment.SHOW_SQL, "true");
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-				settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+    private static SessionFactory sessionFactory;
 
-				// c3p0 configuration
-	            settings.put(Environment.C3P0_MIN_SIZE, 5);         //Minimum size of pool
-	            settings.put(Environment.C3P0_MAX_SIZE, 20);        //Maximum size of pool
-	            settings.put(Environment.C3P0_ACQUIRE_INCREMENT, 1);//Number of connections acquired at a time when pool is exhausted 
-	            settings.put(Environment.C3P0_TIMEOUT, 1800);       //Connection idle time
-	            settings.put(Environment.C3P0_MAX_STATEMENTS, 150); //PreparedStatement cache size
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "123456");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-	            settings.put(Environment.C3P0_CONFIG_PREFIX+".initialPoolSize", 5);
-	            
-				configuration.setProperties(settings);
-				configuration.addAnnotatedClass(Student.class);
-				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-						.applySettings(configuration.getProperties()).build();
-				System.out.println("Hibernate Java Config serviceRegistry created");
-				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-				return sessionFactory;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return sessionFactory;
-	}
+                // c3p0 configuration
+                settings.put(Environment.C3P0_MIN_SIZE, 5);         //Minimum size of pool
+                settings.put(Environment.C3P0_MAX_SIZE, 20);        //Maximum size of pool
+                settings.put(Environment.C3P0_ACQUIRE_INCREMENT, 1);//Number of connections acquired at a time when pool is exhausted
+                settings.put(Environment.C3P0_TIMEOUT, 1800);       //Connection idle time
+                settings.put(Environment.C3P0_MAX_STATEMENTS, 150); //PreparedStatement cache size
+
+                settings.put(Environment.C3P0_CONFIG_PREFIX + ".initialPoolSize", 5);
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(Student.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                System.out.println("Hibernate Java Config serviceRegistry created");
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                return sessionFactory;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 }
